950aac624e5ed99442fdb0881023e682d307b8b1
PostePopote <franciscoloforte@gmail.com>
2025-10-28 11:10:14 -0300

    Primera parte del trabajo

--- src/Productos.jsx
+++ src/Productos.jsx
@@ -1,30 +1,38 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./Productos.css";
 
 export default function Productos() {
   const [productos, setProductos] = useState([]);
 
+  const buscar = () => {
+    fetch('https://fakestoreapi.com/products')
+    .then(response => response.json())
+    .then(data => setProductos(data));
+  }
+
+useEffect(() => buscar,[])
+console.log(productos)
 
   return (
     <div className="productos-container">
       <h2 className="productos-title">Lista de productos</h2>
       <div className="productos-grid">
 
-          { /* Producto de ejemplo:
+          {productos.map((producto) => (
           <div className="producto-card">
             <img
-              src={}
-              alt={}
+              src={producto.image}
+              alt={producto.title}
               className="producto-img"
             />
-            <h3 className="producto-titulo">{}</h3>
-            <p className="producto-categoria">{}</p>
-            <p className="producto-precio">${}</p>
+            <h3 className="producto-titulo">{producto.title}</h3>
+            <p className="producto-categoria">{producto.category}</p>
+            <p className="producto-precio">${producto.price}</p>
             <p className="producto-rating">
               ⭐
             </p>
-          </div> */ }
-
+          </div>  
+))}
       </div>
     </div>
   );
